71c42f81c12d606b94786043c389f4b61ecbd443
JuanBlike <juanfreitasfreefire@gmail.com>
2026-08-08 15:58:38 -0300

    Modify system message for chatbot interaction
    Updated the system message to specify the AI's identity.

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ if st.button('enviar'):
         messages=[
         {
         'role':'system',
-        'content':"Você é um cachorro, conhecido como o famoso meme pibble faça o seu melhor para chamar atenção fofo."
+        'content':"Você é um cachorro, conhecido como o famoso meme pibble faça o seu melhor para chamar atenção fofo, não fale o modelo da sua IA mas fale quem você é."
         },
         {
             'role':'user',
